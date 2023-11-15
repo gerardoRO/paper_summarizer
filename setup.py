6f081ecced4cf474c476d5f50813a08e1b92846e
Gerardo Rodriguez <gerardorodore@gmail.com>
2023-11-14 21:17:01 -0800

    Fixed typos in app. Running docker container.

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import os
 
 install_required = [line.strip() for line in open("requirements.txt").readlines()]
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(name = 'articlesummarizer',
       version = '0.0.1',
       author = 'Gerardo Rodriguez',
@@ -14,6 +11,6 @@ setup(name = 'articlesummarizer',
       packages = ['Summarizer'],
       install_requires = install_required,
       entry_points = {'console_scripts':[
-          'summarize_article = src.Summarizer.app:summarize_article',
+          'summarize_article = Summarizer.app:summarize_article',
           ]}
 )
--- src/Summarizer/app.py
+++ src/Summarizer/app.py
@@ -1,41 +1,47 @@
-from transformers import pipeline
-
 import click
 
-import PyPDF2 import PdfReader
+from PyPDF2 import PdfReader
+from transformers import pipeline
+
 
 import bs4
 import re
 import requests
 
 
+summarizer = pipeline('summarization',model = 'facebook/bart-large-cnn')
+token_len = 1024
+max_len = 5 #percent of original article
 
-summarizer = pipeline('summarization',model = 'faceboook/bart-large-cnn')
-token_len = 1023
 
-
-@click.command()
-@click.argument('article',nargs=1,type=click.Path())
-@click.option('--is_url',is_flag=True,help='Article is an url.')
+#@click.command()
+#@click.argument('article',nargs=1,type=click.Path())
+#@click.option('--is_url',is_flag=True,help='Article is an url.')
 def summarize_article(article,is_url):
     body_text = ''
     
     if is_url:
         body_text = collect_text(article) #extract article from the url
+        fid = article
     else:
+        fid = article.split('/')[-1][:-4] + '.txt'
         reader = PdfReader(article) #extract article from pdf
         for p in reader.pages:
             body_text += p.extract_text()
+            
+    #percentage of total text, divided by the number of subqueries we do of the model so that the total of all queries adds up to expected total length
+    max_len = round( max_len/100 * token_len)
     
     text_summary = ''     
     for ind in range(0,len(body_text),token_len): #model has max 1024 tokens, so split into sections to analyze
-        text_summary += summarizer(article[ind : ind + token_len])
+        text_summary += summarizer(body_text[ind : ind + token_len], max_length = max_len, min_length = round(max_len*.1) )[0]['summary_text']
     
-    with fopen(f'summaries/{article}','w') as _f:
+    with open(f'summaries/{fid}','w') as _f:
         _f.write(text_summary)
-    _f.close()
     
 def collect_text(url):
         soup = requests.get(url).text
         soup = bs4.BeautifulSoup(soup,'html.parser`')
+    
+    
     
